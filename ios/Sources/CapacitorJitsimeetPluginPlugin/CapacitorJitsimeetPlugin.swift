50440ff3d9eeeeee562cac1d848fdc045b0ebc9f
Skynet <nzureuz@gmail.com>
2025-05-03 02:44:28 -0600

    remove comments and a trailing comma to prevent errors on code

--- ios/Sources/CapacitorJitsimeetPluginPlugin/CapacitorJitsimeetPlugin.swift
+++ ios/Sources/CapacitorJitsimeetPluginPlugin/CapacitorJitsimeetPlugin.swift
@@ -3,17 +3,13 @@ import Capacitor
 import JitsiMeetSDK
 import UIKit
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(CapacitorJitsiMeetPlugin)
 public class CapacitorJitsiMeetPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "CapacitorJitsiMeetPlugin"
     public let jsName = "JitsiMeet"
     public let pluginMethods: [CAPPluginMethod] = [
         CAPPluginMethod(name: "joinRoom", returnType: CAPPluginReturnPromise),
-        CAPPluginMethod(name: "leaveRoom", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "leaveRoom", returnType: CAPPluginReturnPromise)
     ]
     private let implementation = CapacitorJitsiMeetPlugin()
 
